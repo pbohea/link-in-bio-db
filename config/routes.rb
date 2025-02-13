11bcbc0bb2d5a4886616717d1f9a434cf4fe504e
pbohea <pbohea@gmail.com>
2025-02-13 16:57:16 +0000

    stopped after grade

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
-get("/home", { :controller => "items", :action => "index" })
+get("/home", { :controller => "pages", :action => "home" })
+get("/", { :controller => "items", :action => "index"})
   
 end
